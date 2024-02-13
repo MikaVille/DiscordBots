2c1213220654c7dbc460fd47908e15a68ce2d62f
NinaTrofimov <159206584+MikaVille@users.noreply.github.com>
2024-02-12 22:00:52 -0500

    Delete ping.js

--- bot_test/commands/ping.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = {
-    data: new SlashCommandBuilder().setName('ping').setDescription("Replies with Pong!"),
-    run: ({ interaction }) =>{
-    interaction.reply('Pong');
-    },
-};
